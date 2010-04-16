d347bf89ca7b2fca741843be73013b720b13dc67
raony <raony@raony-laptop.(none)>
2010-04-16 19:00:51 -0300

    issue 1 closed by this commit

--- naif/graphdb/graphdatabase.py
+++ naif/graphdb/graphdatabase.py
@@ -6,6 +6,91 @@ This is a wrapper module over neo4j
 
 """
 import neo4j
+import pickle
+
+class NodeType(object):
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return self.name
+    
+    def __eq__(self, obj):
+        return (type(obj) == type(self.name) and self.name == obj) or (type(obj) == type(self) and self.name == obj.name)
+    
+    __unicode__ = __str__
+    
+class LinkType(object):
+    BINARY = 1
+    VALUED = 0
+    MIXED = -1
+    
+    def __eq__(self, obj):
+        return (type(obj) == type(self.name) and self.name == obj) or (type(obj) == type(self) and self.name == obj.name)
+    
+    def __init__(self, name, binary):
+        if binary not in [1,0,-1]:
+            raise ValueError("Invalid binary value.")
+        self.name = name
+        self.binary = binary
+    
+    def __str__(self):
+        bstr = ''
+        if self.binary == 0:
+            bstr = 'V'
+        elif self.binary == 1:
+            bstr = 'B'
+        else:
+            bstr = 'M'
+        return '{0} - {1}'.format(self.name, bstr)
+    
+    __unicode__ = __str__
+
+class Link(object):
+    class AlreadyExist(Exception):
+        pass
+    
+    def __init__(self, neo_link):
+        self.__link__ = neo_link
+
+    @property
+    def start(self):
+        return Node(self.__link__.start)
+    
+    @property
+    def end(self):
+        return Node(self.__link__.end)
+    
+    @property
+    def id(self):
+        return self.__link__['net_id']
+    
+    @property
+    def type(self):
+        return self.__link__.type
+    
+    @property
+    def binary(self):
+        return self.__link__['binary']
+
+    @property
+    def strength(self):
+        return self.__link__['strength']
+    
+    def __eq__(self, obj):
+        return type(obj) == type(self) and obj.__link__ == self.__link__
+
+    def __getitem__(self, index):
+        return self.__link__[index]
+    
+    def __setitem__(self, key, value):
+        if key in ['binary', 'strength']:
+            raise KeyError('{0} is a read-only attribute.'.format(key))
+        self.__link__[key] = value
+    
+    def __contains__(self, key):
+        return key in self.__link__
+
 
 class Node(object):
     class AlreadyExist(Exception):
@@ -46,11 +131,18 @@ class GraphDatabase(object):
             logger = None
             
         self.__neo__ = neo4j.GraphDatabase(path, log=logger)
+        self.__neopath__ = path
         self.__node_index__ = self.__neo__.index('node index', create=True)
     
+    def __types__(self, type_attr):
+        types = self.__neo__.ref.get(type_attr, '')
+        return types and pickle.loads(str(types)) or []
+    
     def __node_types__(self):
-        types = self.__neo__.ref.get('node_types', '')
-        return types and types.split(',') or []
+        return self.__types__('node_types')
+    
+    def __link_types__(self):
+        return self.__types__('link_types')
     
     def __nodes_by_type__(self, type):
         class NodeByType(neo4j.Traversal):
@@ -68,14 +160,23 @@ class GraphDatabase(object):
                 return MyIter(super(NodeByType, self).__iter__())
                 
         return NodeByType(self.__neo__.ref)
-    
-    
+
     def add_node_type(self, type):
-        types = self.__node_types__()
+        type = NodeType(type)
+        types = self.__types__('node_types')
         if type not in types:
             types.append(type)
-        self.__neo__.ref['node_types'] = ','.join(types)        
+        self.__neo__.ref['node_types'] = pickle.dumps(types)
     
+    def add_link_type(self, type, binary):
+        type = LinkType(type, binary and LinkType.BINARY or LinkType.VALUED)
+        types = self.__types__('link_types')
+        if type in types and types[types.index(type)].binary != binary:
+            types[types.index(type)].binary = LinkType.MIXED
+        else:
+            types.append(type)
+        self.__neo__.ref['link_types'] = pickle.dumps(types)
+        
     @property
     def node(self):
         gdb = self
@@ -110,6 +211,53 @@ class GraphDatabase(object):
                 gdb.__node_index__[str(id)] = node
                 return Node(node)
         return nodelist()
+    
+    @property
+    def link(self):
+        gdb = self
+        class linklist(object):
+            @property
+            def type(self):
+                class nodetype(object):
+                    def __init__(self):
+                        self.types = gdb.__link_types__()
+                    def __getitem__(self, index):
+                        return self.types[index]
+                    def __contains__(self, obj):
+                        return obj in self.types
+                    def __len__(self):
+                        return len(self.types)
+#                    def __call__(self, type):
+#                        return gdb.__nodes_by_type__(type)
+                return nodetype()
+            
+            def __getitem__(self, key):
+                result = None
+                try:
+                    for rel in gdb.__neo__.relationship:
+                        if rel.get('net_id', None) == key:
+                            result = Link(rel)
+                            break
+                except Exception, e:
+                    if 'No transaction found' in str(e):
+                        raise e
+                return result
+            def __contains__(self, key):
+                return self[key] != None
+            def __call__(self, id, start, end, type='LINK', strength=None, **kwargs):
+                if kwargs.get('binary', None) and strength:
+                    raise ValueError('Binary links cannot have strength.')
+                if id in self:
+                    raise Link.AlreadyExist
+                if strength == None:
+                    binary = True
+                    strength = 1.0
+                else:
+                    binary = False
+                gdb.add_link_type(type, binary)
+                link = start.__node__.__getattr__(type)(end.__node__, net_id=id, binary=binary, strength=strength, **kwargs)
+                return Link(link)
+        return linklist()
 
     def shutdown(self):
         self.__neo__.shutdown()
@@ -117,3 +265,44 @@ class GraphDatabase(object):
     @property
     def transaction(self):
         return self.__neo__.transaction
+#        if self.__transaction__:
+#            return self.__transaction__
+#        
+#        gdb = self
+#        
+#        class MyTransaction(object):
+#            class TransactionClosed(Exception):
+#                pass
+#            def __init__(self):
+#                self.__tx__ = None
+#                self.closed = False
+#                self.failure = False
+#                
+#            def begin(self):
+#                if self.closed:
+#                    raise self.TransactionClosed
+#                if not self.__tx__:
+#                    self.__tx__ = gdb.__neo__.transaction()
+#                    gdb.__begin_transaction__()
+#            def failure(self):
+#                self.__tx__.failure()
+#                gdb.__end_transaction__(False)
+#            def success(self):
+#                self.__tx__.success()
+#                gdb.__end_transaction__(True)
+#            def finish(self):
+#                self.__tx__.finish()
+#                gdb.__transaction__ = None
+#                self.closed = True   
+#            __enter__ = __call__ = begin
+#            def __exit__(self,  type=None, value=None, traceback=None):
+#                if self.__tx is not None:
+#                    if type is None:
+#                        self.success()
+#                    else:
+#                        self.failure()
+#                    self.finish()
+#        
+#        self.__transaction__ = MyTransaction()
+#        return  self.__transaction__
+        
--- naif/graphdb/tests.py
+++ naif/graphdb/tests.py
@@ -3,16 +3,14 @@ Created on Apr 15, 2010
 
 @author: raony
 '''
-from graphdatabase import GraphDatabase, Node
+from graphdb.graphdatabase import GraphDatabase, Node, Link, LinkType
 import unittest
 
 class GraphDatabaseTest(unittest.TestCase):
 
-
     def setUp(self):
         self.graphdb = GraphDatabase('neo_test/', True)
         self.transaction= self.graphdb.transaction()
-
 
     def tearDown(self):
         self.transaction.failure()
@@ -95,6 +93,142 @@ class GraphDatabaseTest(unittest.TestCase):
         self.assertTrue('name' in node)
         self.assertTrue('age' not in node)
     
+    def test_new_link(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(8, start, end)
+        self.assertEquals(start, link.start)
+        self.assertEquals(end, link.end)
+        self.assertEquals(8, link.id)
+        self.assertEquals('LINK', link.type)
+        
+    def test_get_link_by_id(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(12, start, end)
+        self.assertEquals(link.__link__, self.graphdb.link[12].__link__)
+    
+    def test_contains_link_by_id(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        self.graphdb.link(16, start, end)
+        self.assertTrue(16 in self.graphdb.link)
+        
+    def test_not_contains_link_id(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        self.graphdb.link(16, start, end)
+        self.assertTrue(27 not in self.graphdb.link)
+        
+    def test_duplicate_link_exception(self):
+        try:
+            start = self.graphdb.node(1)
+            end = self.graphdb.node(2)
+            self.graphdb.link(16, start, end)
+            self.graphdb.link(16, end, start)
+            self.fail('should raise a Node.AlreadyExist exception')
+        except Link.AlreadyExist:
+            pass
+    
+    def test_link_type_property(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(16, start, end, type='KNOW')
+        self.assertEquals('KNOW', link.type)
+    
+    def test_link_binary_property(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(16, start, end, type='KNOW')
+        self.assertTrue(link.binary)
+        self.assertEquals(1.0, link.strength)
+        
+    def test_link_valued_property(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(16, start, end, type='KNOW', strength=7.5)
+        self.assertTrue(link.binary == False)
+        self.assertEquals(7.5, link.strength)
+        
+    def test_link_generic_properties(self):
+        source = 'e-mail'
+        age = 16
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(16, start, end, source=source, age=age)
+        self.assertEquals(source, link['source'])
+        self.assertEquals(age, link['age'])
+    
+    def test_set_link_generic_properties(self):
+        age = 16
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(16, start, end)
+        link['age'] = age
+        self.assertEquals(age, link['age'])
+
+    def test_set_link_binary_exception(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(16, start, end)
+        try:
+            link['binary'] = False
+            self.fail('should raise a KeyError exception.')
+        except KeyError:
+            self.assertEquals(True, link.binary)
+    
+    def test_set_link_strength_exception(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(16, start, end, strength=6.8)
+        try:
+            link['strength'] = 5.5
+            self.fail('should raise a KeyError exception.')
+        except KeyError:
+            self.assertEquals(6.8, link.strength)
+    
+    def test_contains_link_property(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        link = self.graphdb.link(16, start, end, familiar=True)
+        self.assertTrue('familiar' in link)
+        self.assertTrue('age' not in link)
+    
+    def test_mutually_exclusive_strength_or_binary(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        try:
+            self.graphdb.link(16, start, end, binary=True, strength=2.3)
+            self.fail("should raise ValueError.")
+        except ValueError:
+            pass
+    
+    def test_list_link_types(self):
+        start = self.graphdb.node(1)
+        end = self.graphdb.node(2)
+        self.graphdb.link(16, start, end, type="CHILD")
+        self.graphdb.link(17, start, end, type="FRIEND", strength=3.4)
+        self.assertEquals(2, len(self.graphdb.link.type))
+        self.assertTrue('CHILD' in self.graphdb.link.type)
+        self.assertTrue('FRIEND' in self.graphdb.link.type)
+        self.assertEquals(LinkType.BINARY, filter(lambda x: x == 'CHILD', self.graphdb.link.type)[0].binary)
+        self.assertEquals(LinkType.VALUED, filter(lambda x: x == 'FRIEND', self.graphdb.link.type)[0].binary)
+        self.graphdb.link(18, end, start, type="FRIEND")
+        self.assertEquals(LinkType.MIXED, filter(lambda x: x == 'FRIEND', self.graphdb.link.type)[0].binary)
+        
+    
+#    def test_pickle(self):
+#        n = self.graphdb.node(1)
+#        a = [1, 2, 3]
+#        s1 = pickle.dumps(a)
+#        n['e'] = s1
+#        s2 = n['e']    
+#        
+#        pickle.loads(s1)
+#        pickle.loads(str(s2))
+#        
+#        self.assertEquals(s1, s2)
+            
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testFirstTimeNodeIndexInit']
+#    import sys;sys.argv = ['', 'GraphDatabaseTest.test_pickle']
     unittest.main()
